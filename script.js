7e48b1d62c138910a52ff9b774dccbf4201575fb
Anuj <anujkrgupta21@gmail.com>
2023-11-12 15:42:25 +0530

    side bar fixed, transition and js added

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+const bars = document.querySelector(".bars");
+const leftNavBar = document.querySelector(".leftNavBAr");
+const rightVideosCard =document.querySelector(".rightVideosCard");
 
+bars.addEventListener("click", hideLeftNav);
 
+function hideLeftNav(e) {
+  e.preventDefault();
+  leftNavBar.classList.toggle("hideNav");
 
+  if (leftNavBar.classList.contains("hideNav")) {
+    leftNavBar.style.width = "0";
+    rightVideosCard.style.width = "100%";
+  } else {
+    leftNavBar.style.width = "20%";
+    rightVideosCard.style.width = "80%";
+  }
+}
+bars.addEventListener("click", showLeftNav);
 
-
-
-
-
-
-
-
-
-console.log(obj.proto__);
+function showLeftNav() {}
