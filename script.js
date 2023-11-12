8c3ef429ddeafcbb4ef9be8c35194fa07341af23
Anuj <anujkrgupta21@gmail.com>
2023-11-12 15:48:56 +0530

    header position sticky fixed

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ const bars = document.querySelector(".bars");
 const leftNavBar = document.querySelector(".leftNavBAr");
 const rightVideosCard =document.querySelector(".rightVideosCard");
 
+
+
+// Hide show Side Navbar Function
 bars.addEventListener("click", hideLeftNav);
 
 function hideLeftNav(e) {
   e.preventDefault();
-  leftNavBar.classList.toggle("hideNav");
+  leftNavBar.classList.toggle("hideNav"); //adding a class to hide side nav
 
   if (leftNavBar.classList.contains("hideNav")) {
     leftNavBar.style.width = "0";
-    rightVideosCard.style.width = "100%";
-  } else {
+    rightVideosCard.style.width = "100%"; //making main screen full when nav hidden
+  } 
+  else {
     leftNavBar.style.width = "20%";
-    rightVideosCard.style.width = "80%";
+    rightVideosCard.style.width = "80%";  //making main screen 80% when nav visible
   }
 }
-bars.addEventListener("click", showLeftNav);
 
-function showLeftNav() {}
