d683e48cb8e3d9f2b8731df552c96650bb5c51ab
Anuj <anujkrgupta21@gmail.com>
2023-11-13 19:48:10 +0530

    search box responsive+functionality

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const bars = document.querySelector(".bars");
 const leftNavBar = document.querySelector(".leftNavBAr");
-const rightVideosCard =document.querySelector(".rightVideosCard");
-
-
+const rightVideosCard = document.querySelector(".rightVideosCard");
 
 // Hide show Side Navbar Function
 bars.addEventListener("click", hideLeftNav);
@@ -15,21 +13,56 @@ function hideLeftNav(e) {
     leftNavBar.style.width = "0";
     rightVideosCard.style.width = "100%"; //making main screen full when nav hidden
     // mainScreenFlag = true
-  } 
-  else {
+  } else {
     leftNavBar.style.width = "20%";
-    rightVideosCard.style.width = "80%";  //making main screen 80% when nav visible
+    rightVideosCard.style.width = "80%"; //making main screen 80% when nav visible
     // mainScreenFlag = false;
   }
-//   if (window.innerWidth <= 600) {
-//      console.log('open')
-//       leftNavBar.style.width = "0";
-//       rightVideosCard.style.width = "100%";
-//     } 
-//     else {
-//      console.log('closed');
-//     }
+  //   if (window.innerWidth <= 600) {
+  //      console.log('open')
+  //       leftNavBar.style.width = "0";
+  //       rightVideosCard.style.width = "100%";
+  //     }
+  //     else {
+  //      console.log('closed');
+  //     }
 }
 
+const search = document.querySelector(".fa-magnifying-glass");
+const SearchBar = document.querySelector(".SearchBar");
+const searchInput = document.querySelector(".searchInput");
+const logoRight = document.querySelector(".logoRight");
+const searchBtn = document.querySelector(".searchBtn");
 
+search.addEventListener("click", function (e) {
+  let TargetClass = e.target.classList;
+  console.log(TargetClass);
 
+  if (TargetClass.contains("fa-magnifying-glass")) {
+    console.log("Anuj");
+    logoRight.style.display = "none";
+
+    searchInput.style.display = "block";
+
+    SearchBar.style.marginLeft = "15px";
+
+    searchBtn.style.width = "65px";
+    searchBtn.style.height = "100%";
+    searchBtn.style.backgroundColor = "#303030";
+    searchBtn.style.borderBottomRightRadius = "30px";
+    searchBtn.style.borderTopRightRadius = "30px";
+  }
+});
+
+function updateSizeInfo() {
+  const width = window.innerWidth;
+  // document.getElementById('size-info').textContent = `Current window width: ${width}px`;
+  console.log(`Current window width: ${width}px`);
+  if (width > 600) {
+    console.log("harry");
+  }
+}
+
+// Call the function initially and on window resize
+window.addEventListener("resize", updateSizeInfo);
+updateSizeInfo();
